0bdeb7e008f1e090cfde36839e54fe4cbe2a3c97
Marcos Yuri <marcos.yuriro@gmail.com>
2025-08-05 11:34:40 -0300

    Add files via upload

--- ProjetoMain/src/Main.java
+++ ProjetoMain/src/Main.java
@@ -1,16 +1,19 @@
+import br.com.alura.ProjetoMain.modelos.Filme;
+
 public class Main {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.nome = "O Poderoso Chefão";
-        meuFilme.anoDeLancamento = 1970;
-        meuFilme.duracaoEmMinutos = 180;
+        meuFilme.setNome("O Poderoso Chefão");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
 
         meuFilme.exibaFichaTecnica();
         meuFilme.avalie(7);
         meuFilme.avalie(10);
         meuFilme.avalie(3);
-        System.out.println(meuFilme.somaDasAvaliacoes);
-        System.out.println(meuFilme.totalDeAvaliacoes);
+        System.out.println("Total de Avaliações: " + meuFilme.getTotalDeAvaliacoes());
         System.out.println(meuFilme.pegueMedia());
+        //System.out.println(meuFilme.totalDeAvaliacoes);
+        //System.out.println(meuFilme.pegueMedia());
     }
 }
