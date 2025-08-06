043dcf9248331f6bcb04fcea4a3de0a05afcbf49
Marcos Yuri <marcos.yuriro@gmail.com>
2025-08-06 10:45:03 -0300

    Add files via upload

--- ProjetoMain/src/Main.java
+++ ProjetoMain/src/Main.java
@@ -3,6 +3,7 @@ import br.com.alura.ProjetoMain.modelos.Filme;
 public class Main {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
+        //meuFilme = objeto do tipo Filme
         meuFilme.setNome("O Poderoso Chefão");
         meuFilme.setAnoDeLancamento(1970);
         meuFilme.setDuracaoEmMinutos(180);
@@ -12,7 +13,7 @@ public class Main {
         meuFilme.avalie(10);
         meuFilme.avalie(3);
         System.out.println("Total de Avaliações: " + meuFilme.getTotalDeAvaliacoes());
-        System.out.println(meuFilme.pegueMedia());
+        System.out.println("Média: " + meuFilme.pegueMedia());
         //System.out.println(meuFilme.totalDeAvaliacoes);
         //System.out.println(meuFilme.pegueMedia());
     }
