42862adcc8d0f379c701cf0d3f6941ea766647b2
Marcos Yuri <marcos.yuriro@gmail.com>
2025-08-07 11:21:26 -0300

    Add files via upload

--- ProjetoMain/src/Main.java
+++ ProjetoMain/src/Main.java
@@ -1,14 +1,16 @@
 import br.com.alura.ProjetoMain.modelos.Filme;
+import br.com.alura.ProjetoMain.modelos.Serie;
 
 public class Main {
     public static void main(String[] args) {
+        //meuFilme = objeto do tipo Filme:
         Filme meuFilme = new Filme();
-        //meuFilme = objeto do tipo Filme
         meuFilme.setNome("O Poderoso Chefão");
         meuFilme.setAnoDeLancamento(1970);
         meuFilme.setDuracaoEmMinutos(180);
 
         meuFilme.exibaFichaTecnica();
+        System.out.println("Duração do Filme em minutos: " + meuFilme.getDuracaoEmMinutos());
         meuFilme.avalie(7);
         meuFilme.avalie(10);
         meuFilme.avalie(3);
@@ -16,5 +18,14 @@ public class Main {
         System.out.println("Média: " + meuFilme.pegueMedia());
         //System.out.println(meuFilme.totalDeAvaliacoes);
         //System.out.println(meuFilme.pegueMedia());
+
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2000);
+        lost.exibaFichaTecnica();
+        lost.setTemporadas(10);
+        lost.setEpisodiosPorTemporada(9);
+        lost.setMinutosPorEpisodio(50);
+        System.out.println("Duração da Série: " + lost.getDuracaoEmMinutos());
     }
 }
--- ProjetoMain/src/br/com/alura/ProjetoMain/modelos/Filme.java
+++ ProjetoMain/src/br/com/alura/ProjetoMain/modelos/Filme.java
@@ -1,64 +1,13 @@
 package br.com.alura.ProjetoMain.modelos;
 
-public class Filme {
-    private String nome;
-    private int anoDeLancamento;
-    private boolean incluidoNoPlano;
-    private double somaDasAvaliacoes;
-    private int totalDeAvaliacoes;
-    private int duracaoEmMinutos;
+public class Filme extends Titulo {
+    private String diretor;
 
-    public int getTotalDeAvaliacoes(){
-        return totalDeAvaliacoes;
+    public String getDiretor() {
+        return diretor;
     }
 
-    public String getNome() {
-        return nome;
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
     }
-
-    public int getAnoDeLancamento() {
-        return anoDeLancamento;
-    }
-
-    public boolean getIncluidoNoPlano() {
-        return incluidoNoPlano;
-    }
-
-    public int getDuracaoEmMinutos() {
-        return duracaoEmMinutos;
-    }
-
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setAnoDeLancamento(int anoDeLancamento) {
-        this.anoDeLancamento = anoDeLancamento;
-    }
-
-    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
-        this.incluidoNoPlano = incluidoNoPlano;
-    }
-
-    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
-        this.duracaoEmMinutos = duracaoEmMinutos;
-    }
-
-
-    public void exibaFichaTecnica(){
-        System.out.println("Nome do Filme: " + nome);
-        System.out.println("Ano de lançamento: " + anoDeLancamento);
-    }
-
-    public void avalie(double nota){
-        somaDasAvaliacoes += nota;
-        totalDeAvaliacoes++;
-    }
-
-    public double pegueMedia(){
-        return somaDasAvaliacoes / totalDeAvaliacoes;
-    }
-
-
 }
